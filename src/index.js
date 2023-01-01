8304a8fbc391374cc5b729a03c7fb1ca2eeb7525
veer260 <57021124+veer260@users.noreply.github.com>
2023-01-01 22:25:47 +0530

    First commit with basic setup

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const { PORT } = require('./config/server-config')
+const ApiRoutes = require('./routes/index');
+const db = require('./models/index');
+
+const app = express();
+
+const setupAndStartServer = () => {
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
+    app.use('/api', ApiRoutes);
+    
+
+    app.listen(PORT, () => {
+        if(process.env.DB_SYNC) {
+            db.sequelize.sync({alter: true});
+        }
+        console.log(`Server started at PORT: ${PORT}`);
+    })
+}
+
+setupAndStartServer();
